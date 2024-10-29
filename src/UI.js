d7c226283a5d48ee90a5bba7833a0a33e38eb8ec
Lydos <lydossky@gmail.com>
2024-10-29 16:54:18 +0300

    mockup data parsing

--- src/UI.js
+++ src/UI.js
@@ -1,11 +1,44 @@
 import data from "./projects.json";
+import Project from "./Project.js";
+import Todo from "./Todo";
 
 const contentContainer = document.querySelector("#content-container");
 
 const UI = (function () {
-  let sidebar, projectView;
+  let sidebar,
+    projectView,
+    projects = [];
+
+  function addProject(project) {
+    projects.push(project);
+  }
+
+  function populateProjects() {
+    for (let project of data) {
+      const projectObj = new Project(
+        project.name,
+        project.details,
+        project.createdAt,
+      );
+      projectObj.completed = project.completed;
+      for (let todo of project.todos) {
+        const newTodo = new Todo(
+          todo.title,
+          todo.description,
+          todo.priority,
+          todo.dueDate,
+        );
+        projectObj.addTodo(newTodo);
+      }
+
+      addProject(projectObj);
+    }
+  }
+
 
   function initialView() {
+    populateProjects();
+
     sidebar = document.createElement("div");
     projectView = document.createElement("div");
 
